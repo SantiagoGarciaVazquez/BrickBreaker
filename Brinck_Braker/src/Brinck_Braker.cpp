93881964277054b54a95340496c7816bc1856bf8
SantiagoGarciaVazquez <a23110144@live.ceti.mx>
2024-06-15 17:47:59 -0600

    V1: agregar archivos de la carpeta include y x64

---
+++ Brinck_Braker/src/Brinck_Braker.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "unistd.h"
+#include "GraphicsRunner.hpp"
+
+using namespace sf;
+
+int main(int numArgs, char* args[])
+{
+	// Esto cambia el directorio de trabajo a la ubicación del ejecutable //
+
+	// Obtener la última posición de '/'
+	std::string aux(args[0]);
+
+	// obtenga '/' o '\\' dependiendo de Unix/mac o Windows.
+	#if defined(_WIN32) || defined(WIN32)
+		int pos = aux.rfind('\\');
+	#else
+		unsigned long pos = aux.rfind('/');
+	#endif
+
+	// Obtenga la ruta y cambie el directorio.
+	chdir(aux.substr(0, pos + 1).c_str());
+
+	// Inicialice la ventana con antialiasing y una velocidad de cuadros fija
+	ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Brick Breaker (SAM)", Style::Default, settings);
+	window.setFramerateLimit(60);
+
+	// Cree un corredor de gráficos para contener todos los objetos del juego y manejar el bucle de visualización de dibujo claro.
+	GraphicsRunner game = GraphicsRunner(window);
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			game.handleEvent(event);
+		}
+
+		game.update();
+	}
+	
+	return 0;
+}
